9aa5ad184d70034b460166c9adbb9d97d42ca48f
Muhammad Enamul Haque <enahaque79@gmail.com>
2022-02-23 13:43:25 +0600

    3rd commit

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ const displaySearchResult = meals =>{
    // console.log(meals);
    const searchResult = document.getElementById('search-result');
    meals.forEach(meal =>{
-      console.log(meal);
+      // console.log(meal);
       const div = document.createElement('div');
       div.classList.add('col');
       div.innerHTML = `
